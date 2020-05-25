bec0a06a01eed903835e14d635a2c3e2745bcb6d
Aksqmul01 <63821342+Aksqmul01@users.noreply.github.com>
2020-05-25 13:23:39 +0100

    Basic Trail
    Train moving and sound of it passing by the viewer.
    
    Integration with SQL database.

---
+++ vr-test/Assets/Train/trainMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trainMove : MonoBehaviour {
+
+	void Update()
+	{
+		// Move the object forward along its z axis 1 unit/second.
+		transform.Translate(Vector3.forward * Time.deltaTime);
+
+		// Move the object upward in world space 1 unit/second.
+		transform.Translate(Vector3.forward * Time.deltaTime*2, Space.World);
+	}
+}

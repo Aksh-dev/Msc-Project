c9915bd5d236ec859959f87dbd975740470c8627
Aksqmul01 <63821342+Aksqmul01@users.noreply.github.com>
2020-09-02 09:15:29 +0100

    .

---
+++ Car Simulation/Assets/Runner/Assets/Scripts/CharacterSidewaysMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Assets.Scripts;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CharacterSidewaysMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Animator anim;
+
+    public Transform CharacterGO;
+    
+    void Start()
+    {
+     
+        anim = CharacterGO.GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        anim.SetBool(Constants.AnimationStarted, true);
+
+    }
+
+
+    }
+
+
+
---
+++ Car Simulation/Assets/Scripts/buttonClick.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class buttonClick : MonoBehaviour
+{
+        public GameObject button1;
+        public GameObject button2;
+        public GameObject button3;
+
+    String path = "Assets/Resources/da.txt";
+
+    public void Update()
+        {
+
+      
+        if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject == button1)
+            {
+            
+            //Writing text to the file.
+            TextWriter tsw = new StreamWriter(@"Assets/Resources/data.csv",true);
+            tsw.WriteLine(button1.name);
+            tsw.Close();
+            SceneManager.LoadScene("Menu");
+        }
+        
+        else if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject == button2)
+            {
+            TextWriter tsw = new StreamWriter(@"Assets/Resources/data.csv",true);
+            tsw.WriteLine(button2.name);
+            tsw.Close();
+            SceneManager.LoadScene("Menu");
+        }
+        
+        else if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject == button3)
+            {
+            TextWriter tsw = new StreamWriter(@"Assets/Resources/data.csv", true);
+            tsw.WriteLine(button3.name);
+            tsw.Close();
+            SceneManager.LoadScene("Menu");
+        }
+        }
+    }
+
+
+    
+
